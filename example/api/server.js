b4f5c6f875b3893a6fba2761159ef59b52c4e750
Lars Krieger <lars.krieger@gmail.com>
2016-10-05 09:38:51 +0200

    Ahpify linter on the api server code

--- example/api/server.js
+++ example/api/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
 const Imm = require('immutable');
-const posts = require(__dirname + '/dummy-posts.json');
-const postsWithIncludes = require(__dirname + '/dummy-posts-with-includes.json');
+const posts = require(`${__dirname}/dummy-posts.json`);
+const postsWithIncludes = require(`${__dirname}/dummy-posts-with-includes.json`);
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.get('/api/posts', function (req, res) {
+app.get('/api/posts', (req, res) => {
   if (req.query.include) {
     res.send(postsWithIncludes);
     return;
@@ -18,35 +17,36 @@ app.get('/api/posts', function (req, res) {
   res.send(posts);
 });
 
-app.post('/api/posts', function(req, res) {
+app.post('/api/posts', (req, res) => {
   const responsePosts = Imm.fromJS(posts)
-    .updateIn(['data'], posts => {
-      const latestPost = posts.last();
+    .updateIn(['data'], immPosts => {
+      const latestPost = immPosts.last();
       const newPost = Object.assign(req.body.data, {
         id: parseInt(latestPost.get('id'), 10) + 1
       });
 
-      return posts.push(newPost);
+      return immPosts.push(newPost);
     })
-    .toJS()
-  res.send(responsePosts)
+    .toJS();
+  res.send(responsePosts);
 });
 
-app.delete('/api/posts/:postId', function(req, res) {
+app.delete('/api/posts/:postId', (req, res) => {
   const responsePosts = Imm.fromJS(posts)
-    .updateIn(['data'], posts => {
-      const postIdx = posts.findIndex(post => post.id === req.params.postId);
+    .updateIn(['data'], immPosts => {
+      const postIdx = immPosts.findIndex(post => post.id === req.params.postId);
       if (postIdx === -1) {
-        res.status(404).send('Could not find post, please see request object.')
-        return
+        res.status(404).send('Could not find post, please see request object.');
+        return false;
       }
-      return posts.delete(postIdx);
+
+      return immPosts.delete(postIdx);
     })
-    .toJS()
+    .toJS();
 
   res.send(responsePosts);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
