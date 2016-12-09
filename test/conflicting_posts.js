c5e1b802891bb36edfcd2193a3c84faffa3a4e50
Thomas Harvey <tom@alush.co.uk>
2016-12-09 17:35:57 +0000

    whitespace

--- test/conflicting_posts.js
+++ test/conflicting_posts.js
@@ -11,10 +11,10 @@ describe('[State mutation] Duplicating primary data', () => {
   it('should read and insert data from 2 endpoints into state', () => {
     const stateAfterFirstApiRead = reducer(state, apiRead(posts));
     console.log(stateAfterFirstApiRead.posts);
-    
+
     const stateAfterSecondApiRead = reducer(state, apiRead(extendedPost));
     console.log(stateAfterSecondApiRead.posts);
-    
+
     expect(stateAfterSecondApiRead.posts.data.length).toEqual(posts.data.length);
   });
 });
