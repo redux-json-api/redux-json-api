2e7ddfd457fda1c8480548861a26bf7904772375
Thomas Harvey <tom@alush.co.uk>
2016-12-09 17:46:03 +0000

    explicitly name payload

--- test/link-payload.js
+++ test/link-payload.js
@@ -1,7 +1,8 @@
 import expect from 'expect';
 import { createAction } from 'redux-actions';
 
-import linkedPayload from './payloads/linked_payload.json';
+import postsPayload from './payloads/linked_payload.json';
+
 import { reducer } from '../src/jsonapi';
 
 const state = {};
@@ -9,12 +10,13 @@ const apiRead = createAction('API_READ');
 
 describe('[State mutation] Insertion of links', () => {
   it('should read and insert links into state', () => {
-    const updatedState = reducer(state, apiRead(linkedPayload));
+    const updatedState = reducer(state, apiRead(postsPayload));
 
-    expect(updatedState.posts.data.length).toEqual(linkedPayload.data.length);
-    expect(updatedState.comments.data.length).toEqual(linkedPayload.included.length);
-    expect(updatedState.links.length).toEqual(linkedPayload.links.length);
-    expect(updatedState.links.self).toEqual('http://api-host/api_path/api_endpoint?page%5Bnumber%5D=1&page%5Bsize%5D=100');
-    expect(updatedState.links.next).toEqual('http://api-host/api_path/api_endpoint?page%5Bnumber%5D=2&page%5Bsize%5D=100');
+    expect(updatedState.posts.data.length).toEqual(postsPayload.data.length);
+    expect(updatedState.comments.data.length).toEqual(postsPayload.included.length);
+
+    expect(updatedState.links.length).toEqual(postsPayload.links.length);
+    expect(updatedState.links.self).toEqual('http://api-host/api_path/posts?page%5Bnumber%5D=1&page%5Bsize%5D=2');
+    expect(updatedState.links.next).toEqual('http://api-host/api_path/posts?page%5Bnumber%5D=2&page%5Bsize%5D=2');
   });
 });
