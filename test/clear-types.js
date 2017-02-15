2cda1538cd055637004342a40484af903938bea8
Thomas Harvey <tom@alush.co.uk>
2017-02-15 12:20:01 -0300

    adding test for replacing objects on secondary API reads while maintaining current append behaviour

--- test/clear-types.js
+++ test/clear-types.js
@@ -12,15 +12,27 @@ describe('[State mutation] Testing ClearType', () => {
   it('should append user resources to state on subsequent api reads', () => {
     const state = {};
 
-    const updatedState = reducer(state, apiRead(usersPayloadInitial));
-    expect(updatedState.users.data.length).toEqual(usersPayloadInitial.data.length);
-    expect(updatedState.users.data[0].attributes.name).toEqual("John Doe");
+    const secondaryState = reducer(state, apiRead(usersPayloadInitial));
+    expect(secondaryState.users.data.length).toEqual(usersPayloadInitial.data.length);
+    expect(secondaryState.users.data[0].attributes.name).toEqual('John Doe');
 
-    const tertiaryState = reducer(updatedState, apiRead(usersPayloadFollowup));
+    const tertiaryState = reducer(secondaryState, apiRead(usersPayloadFollowup));
     expect(tertiaryState.users.data.length).toEqual(4);
-    expect(tertiaryState.users.data[2].attributes.github).toEqual("chrisjowen");
+    expect(tertiaryState.users.data[2].attributes.github).toEqual('chrisjowen');
   });
 
   it('should replace user resources in state on subsequent api reads when clearType is set', () => {
+    const state = {};
+
+    const secondaryState = reducer(state, apiRead(usersPayloadInitial));
+    expect(secondaryState.users.data.length).toEqual(usersPayloadInitial.data.length);
+    expect(secondaryState.users.data[0].attributes.name).toEqual('John Doe');
+
+    const replacingPayload = usersPayloadFollowup;
+    replacingPayload.options = { clearTypes: ['users'] };
+
+    const tertiaryState = reducer(secondaryState, apiRead(replacingPayload));
+    expect(tertiaryState.users.data.length).toEqual(2);
+    expect(tertiaryState.users.data[0].attributes.github).toEqual('chrisjowen');
   });
 });
